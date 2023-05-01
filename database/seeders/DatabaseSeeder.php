4e800ebbc1cd0745ba591cd0e015f84743a8d11f
StreetXHasu <69196228+StreetXHasu@users.noreply.github.com>
2023-05-02 00:27:17 +0300

    upgrade to laravel 10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,35 +2,21 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
+        // \App\Models\User::factory(10)->create();
 
-        //тестовый токен
-        DB::table('users')->insert([
-            'name' => 'Test',
-            //  'email' => Str::random(10) . '@gmail.com',
-            'email' => 'Max@gmail.com',
-            'password' => Hash::make('1234567800'),
-        ]);
-        DB::table('personal_access_tokens')->insert([
-            'id' => 1,
-            'tokenable_type' => 'App\Models\User',
-            'tokenable_id' => 1,
-            'name' => 'LaravelSanctumAuth',
-            'token' => 'e65c3a36321f2a50f6e2d43ac29d621ea7b4757ba971053fb2d0b86163da4d02',
-            'abilities' => '["*"]',
-        ]);
-        \App\Models\User::factory(15)->create();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
